6984e256394c6a43581123af467b47e26a06ca03
Sahil Bhirud <sahilbhirud3@gmail.com>
2024-02-07 19:41:47 +0530

    bus booking backend uploaded

---
+++ busbooking/busbooking/src/test/java/com/mybus/busbooking/BusbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybus.busbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
