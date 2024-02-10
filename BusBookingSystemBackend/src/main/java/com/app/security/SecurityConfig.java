a9ca80181375cea4a7da5ee57b2591d26ac08bc0
Sahil Bhirud <sahilbhirud3@gmail.com>
2024-02-10 13:03:21 +0530

    fix(backend): CORS

--- BusBookingSystemBackend/src/main/java/com/app/security/SecurityConfig.java
+++ BusBookingSystemBackend/src/main/java/com/app/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.app.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,66 +15,66 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@EnableWebSecurity//to enable spring sec frmwork support
-@Configuration //to tell SC , this is config class containing @Bean methods
+@EnableWebSecurity
+@Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-//To enable method level authorization support : pre n post authorization
 public class SecurityConfig {
-	//dep : pwd encoder
-	@Autowired
-	private PasswordEncoder enc;
-	//dep : custom jwt auth filter
-	@Autowired
-	private JwtAuthenticationFilter jwtFilter;
-	//dep : custom auth entry point
-	@Autowired
-	private CustomAuthenticationEntryPoint authEntry;
-	
-	
-	@Bean
-	public SecurityFilterChain authorizeRequests(HttpSecurity http) throws Exception
-	{
-		//URL based authorization rules
-		http.cors()
-		.and().
-		//disable CSRF token generation n verification
-		csrf()	.disable()
-		.exceptionHandling().authenticationEntryPoint(authEntry).
-		and().
-		authorizeRequests()
-		.antMatchers("/products/view",
-				
-				"/user/signup",
-				"/user/signin",
-				"/password-reset/reset",
-				"/password-reset/request",
-				
-				"/station/getstations",
-				"/station/deletestation",
-				"/v*/api-doc*/**",
-				"/swagger-ui/**")
-		.permitAll()
-		// only required for JS clnts (react / angular) : for the pre flight requests
-		.antMatchers(HttpMethod.OPTIONS).permitAll()
-		//.antMatchers("/products/purchase").hasRole("CUSTOMER")
-		.antMatchers("/products/add").hasRole("ADMIN")
-		.anyRequest().authenticated()
-		.and()
-		//to tell spring sec : not to use HttpSession to store user's auth details
-		.sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).
-		and()
-		//inserting jwt filter before sec filter
-		.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-	
-		return http.build();
-	}
-	//configure AuthMgr as a spring bean
-	@Bean
-	public AuthenticationManager authenticationManager
-	(AuthenticationConfiguration config) throws Exception
-	{
-		return config.getAuthenticationManager();
-	}
+
+    @Autowired
+    private PasswordEncoder enc;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtFilter;
+
+    @Autowired
+    private CustomAuthenticationEntryPoint authEntry;
+
+    @Bean
+    public SecurityFilterChain authorizeRequests(HttpSecurity http) throws Exception {
+        http.cors().and()
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(authEntry)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/products/view",
+                        "/user/signup",
+                        "/user/signin",
+                        "/password-reset/reset",
+                        "/password-reset/request",
+                        "/station/getstations",
+                        "/station/deletestation",
+                        "/v*/api-doc*/**",
+                        "/swagger-ui/**")
+                .permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/products/add").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3001", "http://localhost:3002"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
