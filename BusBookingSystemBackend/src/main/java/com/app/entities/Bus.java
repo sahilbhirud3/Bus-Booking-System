a382e37d66bc645be104267b56a7087548d20642
Sahil Bhirud <sahilbhirud3@gmail.com>
2024-02-11 11:52:27 +0530

    feat(backend):add get bus controller

--- BusBookingSystemBackend/src/main/java/com/app/entities/Bus.java
+++ BusBookingSystemBackend/src/main/java/com/app/entities/Bus.java
@@ -1,4 +1,4 @@
-package com.app.entities;
+	package com.app.entities;
 
 import java.time.LocalDateTime;
 
--- BusBookingSystemBackend/src/main/java/com/app/service/BusService.java
+++ BusBookingSystemBackend/src/main/java/com/app/service/BusService.java
@@ -1,6 +1,10 @@
 package com.app.service;
 
+import java.util.List;
+
 import com.app.dto.ApiResponse;
+import com.app.dto.GetBusDto;
+import com.app.dto.SendBusDto;
 import com.app.entities.Bus;
 
 
@@ -10,6 +14,6 @@ public interface BusService {
 	
 	ApiResponse removeBus(int busNo);
 	
-	//List<SendBusDto> getBus(GetBusDto gbd);
+	List<SendBusDto> getBuses(GetBusDto gbd);
 
 }
