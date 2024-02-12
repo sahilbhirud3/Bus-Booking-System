39cf59305aef64fe12f828f3c143b32b27dc3691
krishna24102001 <krishnargite1@gmail.com>
2024-02-12 22:04:34 +0530

    minor

--- sparkbusfrontend/src/components/Navbar.jsx
+++ sparkbusfrontend/src/components/Navbar.jsx
@@ -1,61 +1,3 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import { IoMdBus } from "react-icons/io";
-// import { IoNotificationsSharp } from "react-icons/io5";
-// import { CgProfile } from "react-icons/cg";
-// import "./Navbar.css";
-
-// function Navbar() {
-//   // Assume you have a state to manage the JWT token
-//   const [jwtToken, setJwtToken] = useState(localStorage.getItem('jwt'));
-
-//   // Function to handle logout
-//   const handleLogout = () => {
-//     // Perform logout actions (e.g., clear authentication token, reset state, etc.)
-//     localStorage.removeItem('jwt'); // Remove token from localStorage
-//     setJwtToken(null); // Update token state
-//   };
-
-//   return (
-//     <nav className="custom-navbar">
-//       <div className="custom-navbar-container">
-//         <div className="custom-navbar-left">
-//           <div className="logo">
-//             <IoMdBus className='logo-bus'/>
-//           </div>
-//           <h3>SPARK BUS</h3>
-//           <div className='SpaceContainer'/>
-//           <Link to="/" className="dashboard-button">Dashboard</Link>
-//           <Link to="/buslayout" className="team-button">Team</Link>
-//         </div>
-//         <div className="custom-navbar-center">
-//           {/* No content in the center */}
-//         </div>
-//         <div className="custom-navbar-right">
-//           <button className="notification-button">
-//             <IoNotificationsSharp className='notification-icon'/>
-//           </button>
-//           {/* Conditional rendering based on authentication status */}
-//           {jwtToken ? (
-//             <>
-//               <button className="login-button" onClick={handleLogout}>Logout</button>
-//               <button className="profile-button">
-//                 <CgProfile className='profile-icon'/>
-//               </button>
-//             </>
-//           ) : (
-//             <Link to="/login" className="login-button">Login</Link>
-//           )}
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
-
-
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IoMdBus } from "react-icons/io";
@@ -92,7 +34,7 @@ function Navbar() {
           <h3>SPARK BUS</h3>
           <div className='SpaceContainer'/>
           <Link to="/" className="dashboard-button">Dashboard</Link>
-          <Link to="/buslayout" className="team-button">Team</Link>
+          <Link to="/buslcard" className="team-button">Team</Link>
         </div>
         <div className="custom-navbar-center">
           {/* No content in the center */}
