0a71c3c5dcb911b68f4eb1d7ed998b737d5cf61a
krishna24102001 <krishnargite1@gmail.com>
2024-02-15 13:18:21 +0530

    Reverting the blunder

--- sparkbusfrontend/src/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// Create an Axios instance with the specified baseURL
-const axiosInst = axios.create({
-  baseURL: 'https://localhost:7071/', // Set your base URL here
-});
-
-export { axiosInst }; // Export the axiosInstance for use in other files
