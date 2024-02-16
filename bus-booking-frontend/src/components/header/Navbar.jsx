b6a90ef54ecd3bfc50ce3f7d7fe7b1e541ee426b
krishna24102001 <krishnargite1@gmail.com>
2024-02-16 19:19:55 +0530

    n

--- bus-booking-frontend/src/components/header/Navbar.jsx
+++ bus-booking-frontend/src/components/header/Navbar.jsx
@@ -1,80 +1,3 @@
-// import  { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import { IoMdBus } from "react-icons/io";
-// import { IoNotificationsSharp } from "react-icons/io5";
-// import { CgProfile } from "react-icons/cg";
-// import "./Navbar.css";
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-// function Navbar() {
-//   const [jwtToken, setJwtToken] = useState(localStorage.getItem('jwtToken'));
-
-//   // Function to handle logout
-//   const handleLogout = () => {
-//     if (window.confirm("Confirm Logout!")) {
-//         localStorage.removeItem('jwtToken'); // Remove token from localStorage
-//         setJwtToken(null); // Update token state
-//     }
-// };
-
-// const notify = () => {
-//   toast.info("Lets not keep it just a Toast", {
-//     position: "top-right",
-//     autoClose: 3000,
-//     hideProgressBar: false,
-//     closeOnClick: true,
-//     pauseOnHover: true,
-//     draggable: true,
-//     progress: undefined,
-//   });
-// };
-
-
-//   return (
-//     <nav className="custom-navbar">
-//       <div className="custom-navbar-container">
-//         <div className="custom-navbar-left">
-//           <div className="logo">
-//             <IoMdBus className='logo-bus'/>
-//           </div>
-//           <h4>SPARK BUS</h4>
-//           <Link to="/" className="dashboard-button">Dashboard</Link>
-//           <Link to="/aboutus" className="team-button">Team</Link>
-//           {jwtToken && (
-//             <Link to="/bookings" className='bookings-button'>Bookings</Link>
-//           )}
-//         </div>
-        
-//         <div className="custom-navbar-right">
-//         <div className="notification-wrapper">
-//             <button className="notification-button" onClick={notify}>
-//               <IoNotificationsSharp className='notification-icon'/>
-//             </button>
-//             <ToastContainer />
-//           </div>
-          
-          
-          
-//           {jwtToken ? (
-//             <>
-//              <Link className="login-button" onClick={handleLogout}> Logout</Link>
-//               <button className="profile-button">
-//                 <CgProfile className='profile-icon'/>
-//               </button>
-//             </>
-//           ) : (
-//             <Link to="/login" className="login-button">Login</Link>
-//           )}
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
-
-
 // import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 // import { IoMdBus } from "react-icons/io";
@@ -152,8 +75,8 @@
 // }
 
 // export default Navbar;
-import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { IoMdBus } from "react-icons/io";
 import { IoNotificationsSharp } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
@@ -188,6 +111,8 @@ function Navbar() {
       progress: undefined,
     });
   };
+
+  useEffect(()=>{},[]);
 
   return (
     <nav className="custom-navbar">
--- bus-booking-frontend/src/service/axiosInstance.js
+++ bus-booking-frontend/src/service/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // Create an Axios instance with the specified baseURL
 const axiosInst = axios.create({
-  baseURL: 'https://192.168.0.100:7071/', // Set your base URL here
+  baseURL: 'https://localhost:7071/', // Set your base URL here
 });
 
 export { axiosInst }; // Export the axiosInstance for use in other files
