a5004a5dd7fa3676284c14e7f2d21c5c0a349635
Sahil Bhirud <sahilbhirud3@gmail.com>
2024-02-18 13:17:08 +0530

    fix(backend):csrf enabled

--- BusBookingSystemBackend/src/main/java/com/app/security/SecurityConfig.java
+++ BusBookingSystemBackend/src/main/java/com/app/security/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -35,7 +36,9 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain authorizeRequests(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(authEntry).and()
+		http.cors().and()
+		.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and() 
+.exceptionHandling().authenticationEntryPoint(authEntry).and()
 				.authorizeRequests().antMatchers(
 						// user
 						"/user/signup", "/user/signin", "/password-reset/reset", "/password-reset/request",
